3c4fe0bd7dedab2158e34df26d79b1022117a6f0
gechcom <getinetay@gmail.com>
2022-09-08 21:24:46 +0300

    Create stackqueue.c

---
+++ stackqueue.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * op_stack - Set the format to stack (LIFO)
+ * @stack: The stack
+ * @line_number: The line number
+ */
+void op_stack(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+
+	global.queue = 0;
+}
