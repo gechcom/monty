831309e227fbf5316f9348965700369910d58eaf
gechcom <getinetay@gmail.com>
2022-09-08 21:14:22 +0300

    Create mod.c

---
+++ mod.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * op_mod - Mod the second element of the stack by the top element
+ * @stack: The stack
+ * @line_number: The line number
+ */
+void op_mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (!(*stack && (*stack)->next))
+	{
+		dprintf(STDERR_FILENO,
+			"L%u: can't mod, stack too short\n", line_number);
+		free_all(stack);
+		exit(EXIT_FAILURE);
+	}
+	else if ((*stack)->n == 0)
+	{
+		dprintf(STDERR_FILENO,
+			"L%u: division by zero\n", line_number);
+		free_all(stack);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		(*stack)->next->n %= (*stack)->n;
+		temp = *stack;
+		*stack = (*stack)->next;
+		(*stack)->prev = NULL;
+		free(temp);
+	}
+}
